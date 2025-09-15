31fe4e88890aec895bded7d5f73901fa794bcbd5
Azmat Ali <mian.azmatali.9@gmail.com>
2025-09-15 14:58:34 -0400

    HGI-8808: Downgrade requests dependency from 2.32.4 to 2.31.0 to support python 3.7

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(name='tap-braintree',
       py_modules=['tap_braintree'],
       install_requires=[
           'singer-python==5.13.2',
-          'requests==2.32.4',
+          'requests==2.31.0',
           'braintree==4.18.1',
       ],
       extras_require={
